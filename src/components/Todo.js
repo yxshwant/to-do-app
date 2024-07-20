e4774f2f104a6dfe03fa7f06a60e1d10f23f7617
Sarthak Pati <sarthak07.dev@gmail.com>
2024-07-21 03:34:50 +0530

    [Updated] Todo component file.

---
+++ src/components/Todo.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import TodoFilter from "./TodoFilter";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const Todo = () => {
+  const [todos, setTodos] = useLocalStorage("todos", []);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [completedItems, setCompletedItems] = useState(0);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const updateTodo = (index, newText) => {
+    const newTodos = [...todos];
+    newTodos[index].title = newText;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+    const filtered = todos.filter((todo) =>
+      todo.title.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredTodos(filtered);
+  };
+
+  const updateCompletedCount = () => {
+    const completedCount = todos.filter((todo) => todo.isCompleted).length;
+    setCompletedItems(completedCount);
+  };
+  useEffect(() => {
+    updateCompletedCount();
+  }, [todos]);
+
+  return (
+    <div>
+      <h1>
+        📝<u>Todo List</u>
+      </h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoFilter searchTerm={searchTerm} handleSearch={handleSearch} />
+      <TodoList
+        todos={searchTerm ? filteredTodos : todos}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+        toggleComplete={toggleComplete}
+        updateCompletedCount={updateCompletedCount}
+      />
+      <p>
+        Completed Items: {completedItems} / {todos.length}
+      </p>
+    </div>
+  );
+};
+
+export default Todo;
