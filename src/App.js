e8c9003f17dc02ef819688e65b8961b8c12af7f0
Sarthak Pati <sarthak07.dev@gmail.com>
2024-07-21 03:36:40 +0530

    [Update] App.js modify and Routing.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,39 @@
-import React, { useState, useEffect } from "react";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import TodoFilter from "./components/TodoFilter";
-import useLocalStorage from "./hooks/useLocalStorage";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+} from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Todo from "./components/Todo";
 import "./App.css";
 
 const App = () => {
-  const [todos, setTodos] = useLocalStorage("todos", []);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredTodos, setFilteredTodos] = useState([]);
-  const [completedItems, setCompletedItems] = useState(0);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const addTodo = (todo) => {
-    setTodos([...todos, todo]);
+  const handleAuthentication = (authStatus) => {
+    setIsAuthenticated(authStatus);
   };
 
-  const updateTodo = (index, newText) => {
-    const newTodos = [...todos];
-    newTodos[index].title = newText;
-    setTodos(newTodos);
-  };
-
-  const deleteTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
-    setTodos(newTodos);
-  };
-
-  const toggleComplete = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
-    setTodos(newTodos);
-  };
-
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-    const filtered = todos.filter((todo) =>
-      todo.title.toLowerCase().includes(term.toLowerCase())
-    );
-    setFilteredTodos(filtered);
-  };
-
-  const updateCompletedCount = () => {
-    const completedCount = todos.filter((todo) => todo.isCompleted).length;
-    setCompletedItems(completedCount);
-  };
-
-  useEffect(() => {
-    updateCompletedCount();
-  }, [todos]);
   return (
-    <div className="App">
-      <h1>
-        📝<u>Todo List</u>
-      </h1>
-      <TodoForm addTodo={addTodo} />
-      <TodoFilter searchTerm={searchTerm} handleSearch={handleSearch} />
-      <TodoList
-        todos={searchTerm ? filteredTodos : todos}
-        updateTodo={updateTodo}
-        deleteTodo={deleteTodo}
-        toggleComplete={toggleComplete}
-        updateCompletedCount={updateCompletedCount}
-      />
-      <p>
-        Completed Items: {completedItems} / {todos.length}
-      </p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<SignUp></SignUp>}></Route>
+          <Route
+            path="/signin"
+            element={<SignIn onAuth={handleAuthentication}></SignIn>}
+          ></Route>
+          <Route
+            path="/todo"
+            element={isAuthenticated ? <Todo></Todo> : redirect("/signin")}
+          ></Route>
+          <Route path="/" element={<SignUp></SignUp>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
