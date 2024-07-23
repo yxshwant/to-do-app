d4cc308e828dc0053902af7b2dad90532a58529b
Sarthak Pati <sarthak07.dev@gmail.com>
2024-07-24 03:21:41 +0530

    [Fix] index.js (final)

--- backend/index.js
+++ backend/index.js
@@ -30,34 +30,6 @@ const JSON_SERVER_URL = "https://todo-list-8p53.onrender.com/users";
 //   res.json(users);
 // });
 
-// server.post("/signup", (req, res) => {
-//   const { username, password } = req.body;
-//   const users = router.db.get("users").value();
-
-//   if (users.some((user) => user.username === username)) {
-//     return res.status(400).json({ message: "User already exists" });
-//   }
-
-//   const newUser = { username, password };
-//   router.db.get("users").push(newUser).write();
-//   res.status(201).json({ message: "User signed up successfully" });
-// });
-
-// server.post("/signin", (req, res) => {
-//   const { username, password } = req.body;
-//   const users = router.db.get("users").value();
-
-//   const user = users.find(
-//     (user) => user.username === username && user.password === password
-//   );
-
-//   if (user) {
-//     res.status(200).json({ message: "User signed in successfully" });
-//   } else {
-//     res.status(400).json({ message: "Invalid credentials" });
-//   }
-// });
-
 server.post("/users", async (req, res) => {
   console.log(req.body);
   const { action, userData: { username, password } = {} } = req.body;
@@ -73,16 +45,11 @@ server.post("/users", async (req, res) => {
     try {
       const response = await fetch(JSON_SERVER_URL);
       const users = await response.json();
-      // const users = router.db.get("users").value();
-      console.log(users);
 
       if (users.some((user) => user.username === username)) {
         return res.status(400).json({ message: "User already exists" });
       }
-      console.log(username);
-      console.log(password);
-      const newUser = { id: Date.now().toString(), username, password };
-      console.log(newUser);
+      const newUser = { username, password };
       const postResponse = await fetch(JSON_SERVER_URL, {
         method: "POST",
         headers: {
@@ -93,11 +60,15 @@ server.post("/users", async (req, res) => {
       });
 
       const postResponseData = await postResponse.json();
-      console.log('Post response status:', postResponse.status);
-      console.log('Post response data:', postResponseData);
+      console.log("Post response status:", postResponse.status);
+      console.log("Post response data:", postResponseData);
 
       if (!postResponse.ok) {
-        throw new Error(`Failed to create user: ${postResponseData.message || 'Unknown error'}`);
+        throw new Error(
+          `Failed to create user: ${
+            postResponseData.message || "Unknown error"
+          }`
+        );
       }
 
       res.status(201).json({ message: "User signed up successfully" });
@@ -127,30 +98,6 @@ server.post("/users", async (req, res) => {
     }
   }
 });
-
-// server.post("/users", async (req, res) => {
-//   const { action, userData } = req.body;
-//   const { username, password } = userData;
-//   console.log("POST request received at /signin", req.body);
-
-//   try {
-//     const response = await fetch(JSON_SERVER_URL);
-//     const users = await response.json();
-
-//     const user = users.find(
-//       (user) => user.username === username && user.password === password
-//     );
-
-//     if (user) {
-//       res.status(200).json({ message: "User signed in successfully" });
-//     } else {
-//       res.status(400).json({ message: "Invalid credentials" });
-//     }
-//   } catch (error) {
-//     console.error("Error during signin:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
 
 server.use(router);
 server.listen(port, () => {
